0e507926a9372373ac707de5e1e77ab33b546540
Alireza <alireza.k.python1384@gmail.com>
2024-11-05 23:36:53 +0330

    Add search feature

--- school_manager/school/views.py
+++ school_manager/school/views.py
@@ -49,7 +49,7 @@ def education_organization_view(request):
     q = request.GET.get('q', None)
     if q is not None:
         organization_serializer = EducationOrganizationSerializer(
-            EducationOrganization.objects.filter(Q(name__icontains=q) | Q(description__icontians=q)), many=True
+            EducationOrganization.objects.filter(Q(name__icontains=q) | Q(description__icontains=q)), many=True
             )
     else:
         organization_serializer = EducationOrganizationSerializer(EducationOrganization.objects.all(), many=True)
@@ -68,12 +68,22 @@ def student_view(request):
 
 @api_view(['GET'])
 def teacher_view(request):
-    teacher_serializer = TeacherSerializer(Teacher.objects.all(), many=True)
+    q = request.GET.get('q', None)
+    if q is not None:
+        teacher_serializer = TeacherSerializer(
+            Teacher.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q)), many=True
+            )
+    else:   
+        teacher_serializer = TeacherSerializer(Teacher.objects.all(), many=True)
     return Response(teacher_serializer.data)
 
 @api_view(['GET'])
 def master_view(request):
-    master_serializer = MasterSerializer(Master.objects.all(), many=True)
+    q = request.GET.get('q', None)
+    if q is not None:
+        master_serializer = MasterSerializer(Master.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q)), many=True)
+    else:
+        master_serializer = MasterSerializer(Master.objects.all(), many=True)    
     return Response(master_serializer.data)
 
 @api_view(['GET'])
@@ -83,12 +93,20 @@ def class_room_view(request):
 
 @api_view(['GET'])
 def high_student_view(request):
-    high_student_serializer = HighStudentSerializer(HighStudent.objects.all(), many=True)
+    q = request.GET.get('q', None)
+    if q is not None:
+        high_student_serializer = HighStudentSerializer(HighStudent.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q)), many=True)
+    else:
+        high_student_serializer = HighStudentSerializer(HighStudent.objects.all(), many=True)
     return Response(high_student_serializer.data)
 
 @api_view(['GET'])
 def college_student_view(request):
-    college_student_serializer = CollegeStudentSerializer(CollegeStudent.objects.all(), many=True)
+    q = request.GET.get('q', None)
+    if q is not None:
+        college_student_serializer = CollegeStudentSerializer(CollegeStudent.objects.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q)), many=True)
+    else:
+        college_student_serializer = CollegeStudentSerializer(CollegeStudent.objects.all(), many=True)
     return Response(college_student_serializer.data)
 
 @api_view(['GET'])
@@ -103,29 +121,37 @@ def first_high_view(request):
 
 @api_view(['GET'])
 def second_high_view(request):
-
     second_high_serializer = SecondHighSerializer(SecondHigh.objects.all(), many=True)
     return Response(second_high_serializer.data)
 
 @api_view(['GET'])
 def college_view(request):
-
     college_serializer = CollegeSerializer(College.objects.all(), many=True)
     return Response(college_serializer.data)
 
 @api_view(['GET'])
 def snack_view(request):
-
-    snack_serializer = SnackSerializer(Snack.objects.all(), many=True)
+    q = request.GET.get('q' , None)
+    if q is not None:
+        snack_serializer = SnackSerializer(Snack.objects.filter(name__icontains=q), many=True)
+    else:
+        snack_serializer = SnackSerializer(Snack.objects.all(), many=True)
     return Response(snack_serializer.data)
 
 @api_view(['GET'])
 def vending_machine_view(request):
-
-    vending_machine_serializer = VendingMachineSerializer(VendingMachine.objects.all(), many=True)
+    q = request.GET.get('q', None)
+    if q is not None:
+        vending_machine_serializer = VendingMachineSerializer(VendingMachine.objects.filter(shopkeeper__icontains=q), many=True)
+    else:
+        vending_machine_serializer = VendingMachineSerializer(VendingMachine.objects.all(), many=True)
     return Response(vending_machine_serializer.data)
 
 @api_view(['GET'])
 def shop_view(request):
-    shop_serializer = ShopSerializer(Shop.objects.all(), many=True)
+    q = request.GET.get('q', None)
+    if q is not None:
+        shop_serializer = ShopSerializer(Shop.objects.filter(shopkeeper__icontains=q), many=True)
+    else:
+        shop_serializer = ShopSerializer(Shop.objects.all(), many=True)
     return Response(shop_serializer.data)
