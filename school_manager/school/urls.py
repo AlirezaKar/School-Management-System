2c54c017e28e9680eb5ca232e347a840e45d792c
Alireza <alireza.k.python1384@gmail.com>
2024-11-13 20:19:13 +0330

    Add Django drf_yasg api

--- school_manager/school/urls.py
+++ school_manager/school/urls.py
@@ -17,6 +17,25 @@ from school.views import (my_api_view,
                           shop_view,
                           )
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('', home, name='home'),
     path('api/', my_api_view, name='my_api_view'),
@@ -34,4 +53,7 @@ urlpatterns = [
     path('shop/', shop_view, name='shop_view'),
     path('snack/', snack_view, name='snack_view'),
     path('vending-machine/', vending_machine_view, name='vending_machine_view'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
